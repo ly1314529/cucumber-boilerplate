f75380a413b490d7f191ec00fe6dc731b5e91886
ly1314529 <1056510359@qq.com>
2016-02-02 12:01:38 +0800

    Update handleModal.js

--- test/support/action/handleModal.js
+++ test/support/action/handleModal.js
@@ -4,9 +4,9 @@ module.exprots = function (login,password,done){
   return this.browser
        .pause(5000)
        .click(lishengghuoPage.signInButton)
-       .setValue(lishenghuoPage.loginInput,login)
-       .setValue(lishenghuoPage.passwordInput,password)
-       .click(lishenghuoPage.signIn2Button)
+       .setValue('baseurl'.loginInput,login)
+       .setValue('baseurl'.passwordInput,password)
+       .click('baseurl'.signIn2Button)
        .call(done);
  };
 
