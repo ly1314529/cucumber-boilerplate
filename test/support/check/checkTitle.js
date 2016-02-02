566b114ac9605ef43997e1faa77129f064d82302
ly1314529 <1056510359@qq.com>
2016-02-02 14:48:54 +0800

    Update checkTitle.js

--- test/support/check/checkTitle.js
+++ test/support/check/checkTitle.js
@@ -2,14 +2,14 @@
  * check title
  */
 
-module.exports = function (falseCase, docTitle, done) {
+module.exports = function (falseCase, name, done) {
     this.browser
         .getTitle()
         .then(function (title) {
             if (falseCase) {
-                title.should.not.equal(docTitle, 'expected title not to be ' + docTitle);
+                title.should.not.equal(name );
             } else {
-                title.should.equal(docTitle, 'expected title to be "' + docTitle + '"  but found "' + title + '"');
+                title.should.equal( name);
             }
         })
         .call(done);
