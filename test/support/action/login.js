151d2334986fff8c574afa6c531ba7fa5e114d75
ly1314529 <1056510359@qq.com>
2016-02-02 12:03:29 +0800

    Update login.js

--- test/support/action/login.js
+++ test/support/action/login.js
@@ -1,11 +1,12 @@
 module.exports = function (name, password, done) {
     this.browser
+        .clik('baseurl'.login1button)
         .set ({
             name: name,
             password: password
         })
         
         
-        .click('baseurl'.loginbutton)
+        .click('baseurl'.login2button)
         .call(done);
 };
